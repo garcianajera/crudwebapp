0720ac01da3eaa6e1f065a4c7a51615ab02cf8a1
juan <juan@zicral.com>
2018-03-01 14:07:46 -0600

    refactor, using service interface

--- crudwebapp/src/main/java/net/app/service/UserService.java
+++ crudwebapp/src/main/java/net/app/service/UserService.java
@@ -1,24 +1,11 @@
 package net.app.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
 import net.app.entity.User;
-import net.app.repository.UserRepository;
 
-@Service
-public class UserService {
+public interface UserService {
 
-	@Autowired
-	private UserRepository  userRepository;
-	
-	
-	public User addNewUser(User user) {
-		userRepository.save(user);
-		return user;
-	}
-	
-	public User getUserById(Long id) {
-		return userRepository.findOne(id);
-	}
+	User addNewUser(User user);
+
+	User getUserById(Long id);
+
 }
---
+++ crudwebapp/src/main/java/net/app/service/UserServiceImpl.java
@@ -0,0 +1,25 @@
+package net.app.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import net.app.entity.User;
+import net.app.repository.UserRepository;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+	@Autowired
+	private UserRepository userRepository;
+
+	@Override
+	public User addNewUser(User user) {
+		userRepository.save(user);
+		return user;
+	}
+
+	@Override
+	public User getUserById(Long id) {
+		return userRepository.findOne(id);
+	}
+}
